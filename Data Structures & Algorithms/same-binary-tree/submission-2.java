99ffd2d166c991b3b6f162300a6e9e2a6921f134
Nolan Dela Rosa <101160209+26nm@users.noreply.github.com>
2026-04-29 07:33:05 -0700

    Add: same-binary-tree - submission-2

---
+++ Data Structures & Algorithms/same-binary-tree/submission-2.java
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+/**
+* we are given two binary trees p and q
+*
+* return true if trees are equivalent, return false otherwise
+* -trees are said to be the same if they have identical structure
+*  and node values
+*
+* to solve this, we can implement a helper function to compare 
+* both trees:
+* 1. if both roots null -> return true
+*
+* 2. else if one of them null -> return false
+*
+* 3. else if value mismatch -> return false
+*
+* 4. recurse from the left & right for both trees
+*/
+class Solution {
+    // helper to compare subtrees
+    private boolean isSame(TreeNode root1, TreeNode root2) {
+        // if both roots null -> return true
+        if(root1 == null && root2 == null) return true;
+
+        // if one of the roots null -> return false
+        else if(root1 == null || root2 == null) return false;
+
+        // if node values have mismatch -> return false
+        else if(root1.val != root2.val) return false;
+
+        // compare 1st tree's left to 2nd tree's left, and 1st tree's right to 2nd tree's right
+        return (isSame(root1.left, root2.left) 
+                    && isSame(root1.right, root2.right));
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        // return result of helper function
+        return isSame(p, q);
+    }
+}
