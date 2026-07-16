93bb87be7e0fe76253bab07e2cce1d071029050e
Nolan Dela Rosa <101160209+26nm@users.noreply.github.com>
2026-07-16 14:46:00 -0700

    Add: regular-expression-matching - submission-2

---
+++ Data Structures & Algorithms/regular-expression-matching/submission-2.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        // track lengths of both input strings
+        int m = s.length();
+        int n = p.length();
+
+        // create boolean dp array of size m+1 x n+1
+        boolean[][] dp = new boolean[m + 1][n + 1];
+
+        // set very end to true
+        dp[m][n] = true;
+
+        // traverse pattern and string from right to left
+        for(int i = m; i >= 0; i--) {
+            for(int j = n -1; j >= 0; j--) {
+                // determine whether current chars match
+                boolean firstMatch =
+                    i < m &&
+                    (s.charAt(i) == p.charAt(j) ||
+                    p.charAt(j) == '.');
+
+                // if next pattern char is * -> either skip it or
+                    // consume one char if possible
+                if(j + 1 < n && p.charAt(j + 1) == '*') {
+                    dp[i][j] = dp[i][j + 2] ||
+                        (firstMatch && dp[i + 1][j]);
+
+                // otherwise advance both pointers if chars match
+                } else {
+                    dp[i][j] = firstMatch && dp[i + 1][j + 1];
+                }
+            }
+        }
+
+        // return 1st elem in dp
+        return dp[0][0];
+    }
+}
